eb30138a4fc6e5fabbd40e1f8b564f2bac1a0eab
LAB13_USER ON WS19 <L13Y12W19@citulab.net>
2025-04-29 13:11:03 +0800

    fxgl

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module com.oop2.typewiz {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
+    requires com.almasb.fxgl.all;
 
 
     opens com.oop2.typewiz to javafx.fxml;
